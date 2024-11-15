f0944c180dd6bd2364f2df745a5b20b58fd3a7a0
reeasilva <reeasilva@gmail.com>
2024-11-14 21:36:57 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.75.1"
     }
+  }
+  backend "s3" {
+    bucket = "terraform-eks-mm"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
