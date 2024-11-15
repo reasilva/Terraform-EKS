c4bd0c296bf0208accd9a490493743b1a3f9758f
reeasilva <reeasilva@gmail.com>
2024-11-14 23:15:17 -0300

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,11 @@
 resource "aws_vpc" "terraform_eks_mm_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = {
-    Name = "terraform_eks_mm"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name = "terraform_eks_mm"
+    }
+  )
 }
