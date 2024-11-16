3fd683cf50553a199bf48b53e37e883b61d99833
reeasilva <reeasilva@gmail.com>
2024-11-16 13:31:31 -0300

    feat: output subnets

---
+++ modules/network/output.tf
@@ -0,0 +1,12 @@
+output "output_net_subnet_pub_1a" {
+  value = aws_subnet.eks_subnet_public_1a.id
+}
+output "output_net_subnet_pub_1b" {
+  value = aws_subnet.eks_subnet_public_1b.id
+}
+output "output_net_subnet_priv_1a" {
+  value = aws_subnet.eks_subnet_private_1a.id
+}
+output "output_net_subnet_priv_1b" {
+  value = aws_subnet.eks_subnet_private_1b.id
+}
---
+++ output.tf
@@ -0,0 +1,15 @@
+output "output_subnet_pub_1a" {
+  value = module.eks_network.output_net_subnet_pub_1a
+}
+
+output "output_subnet_pub_1b" {
+  value = module.eks_network.output_net_subnet_pub_1b
+}
+
+output "output_subnet_priv_1a" {
+  value = module.eks_network.output_net_subnet_priv_1a
+}
+
+output "output_subnet_priv_1b" {
+  value = module.eks_network.output_net_subnet_priv_1b
+}
