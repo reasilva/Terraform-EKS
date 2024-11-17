4d32701f7705975811139bd39bf61af41acb62ce
reeasilva <reeasilva@gmail.com>
2024-11-16 23:07:10 -0300

    feat: eks vpc config

--- output.tf
+++ output.tf
@@ -13,3 +13,7 @@ output "output_subnet_priv_1a" {
 output "output_subnet_priv_1b" {
   value = module.eks_network.output_net_subnet_priv_1b
 }
+
+output "eks_vpc_config" {
+  value = module.eks_cluster.eks_vpc_config
+}
